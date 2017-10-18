150c87f15d8ca55489a1de709ec55f6d8249a779
john <xiongmeiqiang@xiaomi.com>
2017-10-18 15:47:51 +0800

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,91 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
+import 'antd/dist/antd.css'
+import {Input, Row, Col, Table} from 'antd';
 let yeomanImage = require('../images/yeoman.png');
 
+
+
+// a11=1/E1
+// a22=1/E2
+// a66=1/G12
+// a12= -V12/E2
+// a16= a26=0
+const inputStyle = {
+    margin: '5px 0'
+}
+
+const columns = [
+    {
+        title: 'x',
+        dataIndex: 'x'
+    },
+    {
+        title: 'y',
+        dataIndex: 'y'
+    },
+    {
+        title: `${String.fromCharCode(963)}x`,
+        dataIndex: 'x'
+    },
+    {
+        title: `${String.fromCharCode(963)}y`,
+        dataIndex: 'x'
+    },
+    {
+        title: `${String.fromCharCode(964)}xy`,
+        dataIndex: 'x'
+    },
+]
+let a  = 1;
 class AppComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            E1: 1,
+            E2: 2,
+            G12: 0,
+            V12: 0,
+
+            a: 1,
+            c: 2,
+            w: 0,
+
+            syga: 0,
+
+            a11: 1,
+            a22: 0,
+            a66: 0,
+            a12: 0,
+            a16: 0
+        };
+    }
   render() {
+      const state = this.state;
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <Row>
+            <Col span={4}>
+                <div>
+
+                </div>
+                <div>
+                    <Input type='number' style={inputStyle} value={state.E1} onChange={e=>this.setState({E1:e.target.value})}></Input>
+                    <Input type='number' style={inputStyle} value={state.E2} onChange={e=>this.setState({E2:e.target.value})}></Input>
+                    <Input type='number' style={inputStyle} value={state.G12} onChange={e=>this.setState({G12:e.target.value})}></Input>
+                    <Input type='number' style={inputStyle} value={state.V12} onChange={e=>this.setState({EV12:e.target.value})}></Input>
+                </div>
+
+                <div>
+
+                </div>
+            </Col>
+            <Col span={12} offset={1}>
+                <Table columns={columns}></Table>
+            </Col>
+        </Row>
       </div>
     );
   }
